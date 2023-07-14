713df7feaaeb95d107a78400d7f224580f3328ec
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-07-14 14:42:00 +0300

    completed login page component

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import LoginPage from "../components/LoginPage";
+import Navbar from "../components/Navbar";
 
 const Login = () => {
   return (
     <div>
+      <Navbar />
       <LoginPage />
     </div>
   );
