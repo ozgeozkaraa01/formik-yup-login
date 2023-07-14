f2fe3af569830c17320b796d71e30b18e54ae61b
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-07-14 13:03:22 +0300

    install formik

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -4,8 +4,8 @@ import React from "react";
 const HomePage = () => {
   return (
     <div className="flex-1 bg-gradient-to-r from-teal-400 to-blue-500">
-      <div className="container mx-auto flex justify-center items-center h-screen">
-        <Link href="/">
+      <div className="container mx-auto flex justify-center items-center h-screen space-x-10">
+        <Link href="/login">
           <div className="relative transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
             <img src="/assets/bg1.svg" alt="photo" />
             <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center font-display text-lg">
@@ -13,7 +13,7 @@ const HomePage = () => {
             </span>
           </div>
         </Link>
-        <Link href="/">
+        <Link href="/register">
           <div className="relative transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
             <img src="/assets/bg2.svg" alt="photo" />
             <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center font-display text-lg">
